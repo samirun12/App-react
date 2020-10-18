bedece96b8cfe52b768cabce81d701b3c5db47b4
samirun12 <73050144+samirun12@users.noreply.github.com>
2020-10-18 08:35:49 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -40,6 +40,9 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <p>
+            hello
+          </p>
           <LambdaDemo />
         </header>
       </div>
